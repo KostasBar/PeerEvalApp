35251eb6737cd28af3fb38af513874746800ee94
konbarbounis <konbarbou@gmail.com>
2025-01-23 00:21:30 +0200

    just generated the component

---
+++ PeerEvalApp/PeerEvalAppWeb/peer-eval-app/src/app/components/evaluations-report/evaluations-report.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-evaluations-report',
+  standalone: true,
+  imports: [],
+  templateUrl: './evaluations-report.component.html',
+  styleUrl: './evaluations-report.component.css'
+})
+export class EvaluationsReportComponent {
+
+}
